f75ff368d9a9e92050bd704e166acb9b4bdac8e2
Neil Layne <n.layne@gmail.com>
2015-08-27 21:12:43 -0400

    whitespace, ignore (2 of 2)

--- irccloud.user.js
+++ irccloud.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       irccloud
 // @namespace  http://www.reddit.com/r/creesch
-// @version    0.23
+// @version    0.24
 // @description  do stuff on irccloud!
 // @match      http://*.irccloud.com/*
 // @match      https://*.irccloud.com/*
@@ -12,28 +12,28 @@
 
 function main() {
     var $body = $('body');
-    
+
     //////// subreddit and user linking ////////
     $(document).on('DOMNodeInserted', function(e) {
         $('#limits').remove(); //because it gets replaced every time.
-        
+
         var element = e.target,
             $element = $(element);
 
         if (!$element.hasClass('type_buffer_msg')) {
             return;
         }
-        
+
         var content = $element.find('.content').html();
         if (content) {
             var newcontent = content.replace(/(?:^|[^\w])(\/(u|r)\/\w+)/g, ' <a href="https://www.reddit.com$1" target="_blank">$1</a>');
-            
+
             $element.find('.content').html(newcontent);
             $element.find('.content').addClass('userscript');
         }
     });
-	
-	// when changing channels also insert /u/ and /r/ links as well as dismissing the read buffer.
+
+    // when changing channels also insert /u/ and /r/ links as well as dismissing the read buffer.
     $body.on('click', 'li.buffer', function() {
         $('.content').each(function() {
             if (!$(this).hasClass('userscript')) {
@@ -49,7 +49,7 @@ function main() {
     });
 
 
-	//////// irc markdown ////////
+    //////// irc markdown ////////
     /*
 	***bold italic***
 	**bold**
@@ -58,36 +58,36 @@ function main() {
 	^03color^  <--The number is a color code (user 00 till 15)
 	^03,06colorbackground^ <-- same as above but with a background 
     */
-   $body.on('keyup', function(e) {
+    $body.on('keyup', function(e) {
         $checkbox = $body.find('.markdown:visible');
         var checkboxChecked = $body.find('.markdown:visible:checked').length;
-            
-            if(e.ctrlKey && e.keyCode == 81) {
-                if (checkboxChecked) { 
-                    $checkbox.prop( "checked", false );
-                    checkboxChecked = false;
-                } else { 
-                    $checkbox.prop( "checked", true );
-                    checkboxChecked = true;
-                }
+
+        if (e.ctrlKey && e.keyCode == 81) {
+            if (checkboxChecked) {
+                $checkbox.prop("checked", false);
+                checkboxChecked = false;
+            } else {
+                $checkbox.prop("checked", true);
+                checkboxChecked = true;
             }
-     });
-    
+        }
+    });
+
     $body.delegate('[id^=bufferInputView]:visible', 'click', function() {
         var $this = $(this);
         var $checkbox = $this.closest('form').find('.markdown');
-        if(!$checkbox.length) { 
-            $this.after('<input type="checkbox" class="markdown" style="float: right" name="markdown" value="markdown" checked>'); 
+        if (!$checkbox.length) {
+            $this.after('<input type="checkbox" class="markdown" style="float: right" name="markdown" value="markdown" checked>');
             $this.css('width', 'calc(100% - 13px)');
         }
-        
+
         $this.on('keydown', function(e) {
             var checkboxChecked = $body.find('.markdown:visible:checked').length;
             //console.log(checkboxChecked);
             //console.log(e);
             if (e.keyCode === 13 && checkboxChecked) {
                 e.preventDefault();
-                
+
                 e.currentTarget.value = e.currentTarget.value.replace(/\*\*\*(.*?)\*\*\*/g, '\x1D\x02$1\x0F');
                 e.currentTarget.value = e.currentTarget.value.replace(/\*\*(.*?)\*\*/g, '\x02$1\x0F');
                 e.currentTarget.value = e.currentTarget.value.replace(/\*(.*?)\*/g, '\x1D$1\x0F');
@@ -97,36 +97,36 @@ function main() {
             }
         });
     });
-    
-    
+
+
     //////// Hide inactive ////////
     var delay = 1000,
         enabled = false,
         stickies = JSON.parse(localStorage['IRCC.stickies'] || '[]'),
         hideInactive = JSON.parse(localStorage['IRCC.hideInactive'] || 'false'),
         intId;
-    
+
     $('#sidebar').prepend('<div id="buffersFooter" style="display: block;"><p><a class="tb-hide-inactive" href="javascript:;" style="text-align: center;"><span class="icon"></span>hide inactive</a></p></div>');
-    
+
     // wait for chann list to load
     setTimeout(function() {
         $('li.buffer a.buffer').each(function() {
             var $this = $(this),
                 checked = (stickies.indexOf($this.prop('title')) !== -1);
-            
-            $this.after('<input type="checkbox" class="tb-sticky-chan" style="float: right;"'+ (checked ? ' checked' : '') +'/></input>');
+
+            $this.after('<input type="checkbox" class="tb-sticky-chan" style="float: right;"' + (checked ? ' checked' : '') + '/></input>');
         });
-        
+
         if (hideInactive) {
             toggleInactive();
         }
     }, 5000);
-    
+
     $body.on('click', '.tb-sticky-chan', function() {
         var $this = $(this),
             name = $this.prev().attr('title');
-        
-        if ($this.prop('checked')){
+
+        if ($this.prop('checked')) {
             stickies.push(name);
         } else {
             var idx = stickies.indexOf(name);
@@ -134,53 +134,53 @@ function main() {
                 stickies.splice(idx, 1);
             }
         }
-        
+
         localStorage['IRCC.stickies'] = JSON.stringify(stickies);
         console.log(stickies);
     });
-    
+
     function hideInactiveChanns() {
         $('ul.buffers .active:not(.unread)').each(function() {
             var $this = $(this);
-            
+
             if (stickies.indexOf($this.find('a.buffer').prop('title')) === -1) {
                 $this.hide();
             }
         });
-        
+
         $('ul.buffers .active.unread').show();
-        $('ul.buffers .active.selected').show();  // always show the chann we're in.
+        $('ul.buffers .active.selected').show(); // always show the chann we're in.
     }
-    
+
     function toggleInactive() {
         var $this = $('.tb-hide-inactive');
-        
+
         if (!enabled) {
             // don't show other clutter.
             $('.archiveToggle.show').hide();
             $('.disconnected').hide();
             $('#addNetwork').hide();
             $('#reorderNetworks.show').hide();
-            
+
             $this.text('show all');
-            intId = setInterval(hideInactiveChanns, delay); 
+            intId = setInterval(hideInactiveChanns, delay);
             hideInactiveChanns();
         } else {
             $('.archiveToggle.show').show();
             $('.disconnected').show();
             $('#addNetwork').show();
             $('#reorderNetworks.show').show();
-            
+
             $this.text('hide inactive');
             clearInterval(intId);
             $('ul.buffers .active:not(.unread)').show();
         }
-        
+
         //save it
         enabled = !enabled;
         localStorage['IRCC.hideInactive'] = JSON.stringify(enabled);
     }
-    
+
     $body.on('click', '.tb-hide-inactive', function() {
         toggleInactive();
     });
